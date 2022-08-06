b3d8e14a1ade11baa42e17ed9d60165d267836bd
deewhy2 <lawaldone@gmail.com>
2022-08-06 14:48:05 +0100

    To be contiued with VA list

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _printf - prints anything
+ * @format: the format string
+ * Author: Lawal Oladayo
+ * 
+ * Return: returns the number of characters printed 
+ */
+int printf(const char *format, ...)
+{
+    
+}
