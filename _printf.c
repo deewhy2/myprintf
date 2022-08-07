a6463412b9d2eced2e85049a4c2d04c174df6799
Lawal Mukhtar Oladayo <lawaldone@gmail.com>
2022-08-07 09:53:19 -0700

    Dee

--- _printf.c
+++ _printf.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdarg.h>
+#include <stddef.h>
 
 /**
  * _printf - prints anything
@@ -10,24 +11,43 @@
  */
 int _printf(const char *format, ...)
 {
-va_list args;
-int i = 0;
-    va_start(args, format);
+	va_list args;
+	int i = 0, k = 0;
+	char *str = NULL;
 
-while (format[i] != '\0')
-    {
-	if (format[i] != '%')
-		_putchar(format[i]);
-    else
-    {
-	if (format[i + 1] == 'c')
-		_putchar(va_arg(args, int));
-		i++;
-    }
-    i++;
-    }
+	va_start(args, format);
 
-    va_end(args);
+	while (format[i] != '\0')
+	{
+		if (format[i] != '%')
+			_putchar(format[i]);
+		else
+		{
+			if (format[i + 1] == 'c')
+			{
+				_putchar(va_arg(args, int));
+				i++;
+			}
+			else if (format[i + 1] == 's')
+			{
+				i++;
+				str = va_arg(args, char *);
+				k = 0;
+				while (str[k] != '\0')
+				{
+					_putchar(str[k]);
+					k++;
+				}
+			}
+			else if (format[i + 1] == '%')
+			{
+				i++;
+				_putchar('%');
+			}
+		}
+		i++
+	}
+	va_end(args);
 
-    return (0);
+	return (0);
 }
