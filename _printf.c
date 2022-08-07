4f7ce3d1042a4e909b124ad542fb733d11968dea
Lawal Mukhtar Oladayo <lawaldone@gmail.com>
2022-08-07 10:08:05 -0700

    All Errors fixed

--- _printf.c
+++ _printf.c
@@ -11,51 +11,11 @@
  */
 int _printf(const char *format, ...)
 {
-<<<<<<< HEAD
 	va_list args;
 	int i = 0, k = 0;
 	char *str = NULL;
 
 	va_start(args, format);
-=======
-va_list args;
-int i = 0, k = 0;
-char *str = NULL;
-    va_start(args, format);
-
-while (format[i] != '\0')
-    {
-	if (format[i] != '%')
-		_putchar(format[i]);
-    else
-        {
-            if (format[i + 1] == 'c')
-            {
-                _putchar(va_arg(args, int));
-                i++;
-            }
-            else if (format[i + 1] == 's')
-            {
-                i++;
-                str = va_arg(args, char *);
-                k=0;
-                while (str[k] != '\0')
-                {
-                    _putchar(str[k]);
-                    k++;
-                }
-                
-            }
-            else if(format [i+1] == '%')
-            {
-                i++;
-                _putchar('%');
-            }
-        }
-    
-    i++;
-    }
->>>>>>> 685534bc715d2055809c1f75190d1d906b72fed5
 
 	while (format[i] != '\0')
 	{
@@ -85,14 +45,9 @@ while (format[i] != '\0')
 				_putchar('%');
 			}
 		}
-		i++
+		i++;
 	}
 	va_end(args);
 
-<<<<<<< HEAD
 	return (0);
 }
-=======
-    return (0);
-}
->>>>>>> 685534bc715d2055809c1f75190d1d906b72fed5
